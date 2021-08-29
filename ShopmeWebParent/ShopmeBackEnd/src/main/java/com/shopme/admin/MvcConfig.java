de41bc48ce40d403e1fd95f748be9535001c73a6
minhthong774 <minhthong774@gmail.com>
2021-08-29 21:08:10 +0700

    List Categories in Hierarchical Form and showing image in categories page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -19,6 +19,14 @@ public class MvcConfig implements WebMvcConfigurer {
 		
 		registry.addResourceHandler("/" + dirName + "/**")
 		.addResourceLocations("file://" + userPhotosPath + "/");
+
+		String categoryImagesDirName = "../category-images";
+		Path categoryImageDir = Paths.get(categoryImagesDirName);
+		
+		String categoryImagePath = categoryImageDir.toFile().getAbsolutePath();
+		
+		registry.addResourceHandler("/category-images/**")
+		.addResourceLocations("file://" + categoryImagePath + "/");
 	}
 	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -7,10 +7,8 @@ import java.util.Set;
 import com.shopme.common.entity.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 
 @Service
@@ -21,7 +19,36 @@ public class CategoryService {
     public CategoryRepository categoryRepo;
 
     public List<Category> listAll(){
-        return (List<Category>) categoryRepo.findAll(Sort.by("name").ascending());
+        List<Category> rootCategories = categoryRepo.findRootCategories();
+        return listHierarchicalCategories(rootCategories);
+    }
+
+    private List<Category> listHierarchicalCategories(List<Category> rootCategories){
+        List<Category> hierarchicalCategories = new ArrayList<>();
+
+        for (Category rootCategory : rootCategories){
+            hierarchicalCategories.add(Category.copyFull(rootCategory));
+
+            listSubHierarchicalCategories(hierarchicalCategories, rootCategory, 0);
+        }
+
+        return hierarchicalCategories;
+    }
+
+    private void listSubHierarchicalCategories(List<Category> hierarchicalCategories, Category parent, int subLevel){
+        Set<Category> children = parent.getChildren();
+        int newSubLevel = subLevel + 1;
+        for(Category subCategory : children){
+            String name = "";
+            for(int i = 0; i < newSubLevel; i++){
+                name+="--";
+            }
+            name  += subCategory.getName();
+
+            hierarchicalCategories.add(Category.copyFull(subCategory, name));
+
+            listSubHierarchicalCategories(hierarchicalCategories, subCategory, newSubLevel);
+        }
     }
 
     public Category save(Category category){
@@ -39,14 +66,14 @@ public class CategoryService {
 
                 Set<Category> children = category.getChildren();
 
-                listChildren(categoriesUsedInForm, category, 0);
+                listSubCategoriesUsedInForm(categoriesUsedInForm, category, 0);
             }
         }
 
         return categoriesUsedInForm;
     }
 
-    private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel){
+    private void listSubCategoriesUsedInForm(List<Category> categoriesUsedInForm, Category parent, int subLevel){
         int newSubLevel = subLevel + 1;
         Set<Category> children = parent.getChildren();
 
@@ -59,7 +86,7 @@ public class CategoryService {
 
             categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
 
-            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+            listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
 
         }
     }
