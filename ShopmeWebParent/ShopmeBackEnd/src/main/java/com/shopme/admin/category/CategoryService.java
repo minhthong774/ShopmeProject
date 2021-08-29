4825fb06a5f6e7795582571560226f1e4dc3d694
minhthong774 <minhthong774@gmail.com>
2021-08-29 17:29:22 +0700

    added category_form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -1,6 +1,8 @@
 package com.shopme.admin.category;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import com.shopme.common.entity.Category;
 
@@ -20,4 +22,48 @@ public class CategoryService {
     public List<Category> listAll(){
         return (List<Category>) categoryRepo.findAll(Sort.by("name").ascending());
     }
+
+    public List<Category> listCategoriesUsedInform(){
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+
+        Iterable<Category> categoriesInDB = categoryRepo.findAll();
+
+        for (Category category : categoriesInDB){
+            if(category.getParent()==null){
+                categoriesUsedInForm.add(new Category(category.getName()));
+
+                Set<Category> children = category.getChildren();
+
+                listChildren(categoriesUsedInForm, category, 0);
+
+                // for(Category subCategory : children) {
+                //     String name = "--" + subCategory.getName();
+                //     categoriesUsedInForm.add(new Category(name));
+
+                //     listChildren(categoriesUsedInForm, subCategory, 1);
+                // }
+            }
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel){
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+
+        for(Category subCategory : children){
+            String name = "";
+            for(int i = 0; i < newSubLevel; i++){
+                name+="--";
+            }
+            name  += subCategory.getName();
+
+            categoriesUsedInForm.add(new Category(name));
+
+            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+
+        }
+    }
+
 }
