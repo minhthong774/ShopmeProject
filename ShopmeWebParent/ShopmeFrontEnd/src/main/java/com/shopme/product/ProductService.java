3445f0a4c6ff0aa466d4f9ea4cd1fca977274375
minhthong774 <minhthong774@gmail.com>
2021-09-07 15:27:46 +0700

    Code Search Product

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
     public static final int PRODUCTS_PER_PAGE = 1;
+    public static final int SEARCH_RESULTS_PER_PAGE = 1;
     @Autowired private ProductRepository repo;
 
     public Page<Product> listByCategory(int pageNum, Integer categoryId){
@@ -28,5 +29,10 @@ public class ProductService {
         }
 
         return product;
+    }
+
+    public Page<Product> search(String keyword, int pageNum){
+        Pageable pageable = PageRequest.of(pageNum - 1, SEARCH_RESULTS_PER_PAGE);
+        return repo.search(keyword, pageable);
     }
 }
